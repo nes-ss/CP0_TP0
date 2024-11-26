d7f0142f3c65ffdb5aa59f4b94551cf0153b0907
ayrou <ayrou@10.3.60.229>
2024-11-26 20:52:20 +0100

    Création d'un nouveau projet versionné

--- Exo1/src/exo1/Exo1.java
+++ Exo1/src/exo1/Exo1.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package Exo1;
+package exo1;
 
 import java.util.Scanner;
 

3e96e3b7a95fa3d7e9d1e5c8569f649fd52464de
ayrou <ayrou@10.3.60.229>
2024-11-27 00:42:17 +0100

    Création d'un nouveau projet versionné

---
+++ TP2_Bieres_AYROUR/src/tp2_bieres_ayrour/TP2_Bieres_AYROUR.java
@@ -0,0 +1,31 @@
+package tp2_bieres_ayrour;
+
+import tp2_bieres_ayrour.BouteilleBiere;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author ayrou
+ */
+public class TP2_Bieres_AYROUR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls",(float)7.0 ,"Dubuisson", true) ;
+    BouteilleBiere Leffe = new BouteilleBiere("Abbaye de Leffe",(float)6.6,"Leffe", true);
+    BouteilleBiere test = new BouteilleBiere("testtest",(float)6.6,"TEST", true);
+    System.out.println(uneBiere);
+    System.out.println(Leffe);
+    System.out.println(test);
+    test.Decapsuler();
+    test.Decapsuler();
+    
+    }
+    
+}

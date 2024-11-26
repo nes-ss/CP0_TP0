935ca9d28039aaedd48b7d20ab066cbafb677aa1
ayrou <ayrou@10.3.60.229>
2024-11-27 00:40:16 +0100

    Création d'un nouveau projet versionné

---
+++ TP1_Syntaxe_de_base/src/tp1_syntaxe_de_base/TP1_Syntaxe_de_base.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_syntaxe_de_base;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ayrou
+ */
+public class TP1_Syntaxe_de_base {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Saisisez un nombre : ");
+        int var1=sc.nextInt();
+        System.out.println("Saisisez un deuxieme nombre : ");
+        int var2=sc.nextInt();
+        
+        int res1, res2, res3, res4, res5;
+        res1 = var1 + var2;
+        res2 = var1 - var2;
+        res3 = var1*var2;
+        System.out.println(res1);
+        System.out.println(res2);
+        System.out.println(res3);
+        res4 = var1/var2;
+        res5 = var1%var2;
+        System.out.println(res4);
+        System.out.println(res5);
+        
+        
+        
+        
+    }
+    
+}

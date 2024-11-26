c2fe7977d032341df241546b79fc1def0be0f11e
ayrou <ayrou@10.3.60.229>
2024-11-27 00:47:10 +0100

    Création d'un nouveau projet versionné

---
+++ TP2_relation_1_AYROUR/src/tp2_relation_1_ayrour/Voiture.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_relation_1_ayrour;
+
+/**
+ *
+ * @author ayrou
+ */
+public class Voiture {
+    public String Modele;
+    public int Puissance;
+    public String Voiture;
+    public Personne personne;
+    public Personne propietaire;
+    
+    public Voiture(String modele, String voiture, int puis){
+    this.Modele = modele;
+    this.Voiture = voiture;  
+    this.Puissance = puis;
+    this.propietaire = null;
+    
+}
+    @Override
+    public String toString() {
+        String chaine_a_retourner;
+        chaine_a_retourner = "Object : "+Modele+" "+Voiture+" "+ Puissance;
+        return chaine_a_retourner;
+}
+}

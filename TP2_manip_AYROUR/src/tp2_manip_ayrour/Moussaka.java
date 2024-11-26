d0767d1c33711d32c0c6a7810c897221bf661332
ayrou <ayrou@10.3.60.229>
2024-11-27 00:44:44 +0100

    Création d'un nouveau projet versionné

---
+++ TP2_manip_AYROUR/src/tp2_manip_ayrour/Moussaka.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_ayrour;
+
+/**
+ *
+ * @author ayrou
+ */
+public class Moussaka{
+    public int nbCalories;
+    
+    public Moussaka(int calories){
+        this.nbCalories = calories;
+                }
+ 
+}

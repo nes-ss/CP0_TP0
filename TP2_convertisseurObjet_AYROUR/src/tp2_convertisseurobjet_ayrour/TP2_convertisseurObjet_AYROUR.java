ea8a8836951d9f631059ae415b91d2bb1d3fd1d6
ayrou <ayrou@10.3.60.229>
2024-11-27 00:43:24 +0100

    Création d'un nouveau projet versionné

---
+++ TP2_convertisseurObjet_AYROUR/src/tp2_convertisseurobjet_ayrour/TP2_convertisseurObjet_AYROUR.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_convertisseurobjet_ayrour;
+
+/**
+ *
+ * @author ayrou
+ */
+public class TP2_convertisseurObjet_AYROUR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Convertisseur convertisseur1 = new Convertisseur();
+    }
+    
+}
